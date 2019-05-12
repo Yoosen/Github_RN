04e53bcc957ff5219cfbaba2828a16c4fc88c335
Yoosen <iyoosen@gmail.com>
2019-05-12 12:01:04 +0800

    Initial commit

---
+++ js/navigator/AppNavigators.js
@@ -0,0 +1,38 @@
+import {createStackNavigator, createMaterialTopTabNavigator, createBottomTabNavigator, createSwitchNavigator} from "react-navigation";
+
+import WelcomePage from '../page/WelcomePage';
+import HomePage from '../page/HomePage';
+import DetailPage from '../page/DetailPage';
+
+const InitNavigator = createStackNavigator({
+    WelcomePage:{
+        screen: WelcomePage,
+        navigationOptions:{
+            header: null, //将header设置为null，禁用createStackNavigator的Navigation Bar，全                    屏显示
+        }
+    }
+});
+
+const MainNavigator = createStackNavigator({
+    HomePage:{
+        screen: HomePage,
+        navigationOptions:{
+            header: null, //将header设置为null，禁用createStackNavigator的Navigation Bar，全                    屏显示
+        },
+    },
+    DetailPage:{
+        screen: DetailPage,
+        navigationOptions:{
+            header: null, //将header设置为null，禁用createStackNavigator的Navigation Bar，全                    屏显示
+        },
+    }
+});
+
+export default createSwitchNavigator({
+    Init: InitNavigator,
+    Main: MainNavigator,
+},{
+    navigationOptions:{
+        header: null, //将header设置为null，禁用createStackNavigator的Navigation Bar，全                    屏显示
+    }
+})
