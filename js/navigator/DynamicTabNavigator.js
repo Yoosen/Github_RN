b96f12d5ee37c534ff82193616d21a2830c11224
Yoosen <iyoosen@gmail.com>
2019-05-15 11:20:14 +0800

    解决react-navigation-tabs问题

--- js/navigator/DynamicTabNavigator.js
+++ js/navigator/DynamicTabNavigator.js
@@ -6,9 +6,19 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {createBottomTabNavigator, createAppContainer} from "react-navigation";
+import React, {
+  Component
+} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  createBottomTabNavigator,
+  createAppContainer
+} from "react-navigation";
 import PopularPage from '../page/PopularPage';
 import TrendingPage from '../page/TrendingPage';
 import FavoritePage from '../page/FavoritePage';
@@ -16,56 +26,99 @@ import MyPage from '../page/MyPage';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import NavigationUtil from '../navigator/NavigationUtil';
+import {
+  BottomTabBar
+} from 'react-navigation-tabs';
 
 const TABS = {
-  PopularPage:{
+  PopularPage: {
     screen: PopularPage,
-    navigationOptions:{
+    navigationOptions: {
       tabBarLabel: "首页",
-      tabBarIcon: ({tintColor,focused})=> (
-        <AntDesign
-          name={'home'}
-          size={26}
-          style={{color: tintColor}}
+      tabBarIcon: ({
+        tintColor,
+        focused
+      }) => ( <
+        AntDesign name = {
+          'home'
+        }
+        size = {
+          26
+        }
+        style = {
+          {
+            color: tintColor
+          }
+        }
         />
       ),
     }
   },
-  TrendingPage:{
+  TrendingPage: {
     screen: TrendingPage,
-    navigationOptions:{
+    navigationOptions: {
       tabBarLabel: "问医生",
-      tabBarIcon: ({tintColor,focused})=> (
-        <MaterialCommunityIcons
-          name={'doctor'}
-          size={26}
-          style={{color: tintColor}}
+      tabBarIcon: ({
+        tintColor,
+        focused
+      }) => ( <
+        MaterialCommunityIcons name = {
+          'doctor'
+        }
+        size = {
+          26
+        }
+        style = {
+          {
+            color: tintColor
+          }
+        }
         />
       ),
     }
   },
-  FavoritePage:{
+  FavoritePage: {
     screen: FavoritePage,
-    navigationOptions:{
+    navigationOptions: {
       tabBarLabel: "家人",
-      tabBarIcon: ({tintColor,focused})=> (
-        <AntDesign
-          name={'addusergroup'}
-          size={26}
-          style={{color: tintColor}}
+      tabBarIcon: ({
+        tintColor,
+        focused
+      }) => ( <
+        AntDesign name = {
+          'addusergroup'
+        }
+        size = {
+          26
+        }
+        style = {
+          {
+            color: tintColor
+          }
+        }
         />
       ),
     }
   },
-  MyPage:{
+  MyPage: {
     screen: MyPage,
-    navigationOptions:{
+    navigationOptions: {
       tabBarLabel: "我的",
-      tabBarIcon: ({tintColor,focused})=> (
-        <MaterialCommunityIcons
-          name={'face-profile'}
-          size={26}
-          style={{color: tintColor}}
+      tabBarIcon: ({
+        tintColor,
+        focused
+      }) => ( <
+        MaterialCommunityIcons name = {
+          'face-profile'
+        }
+        size = {
+          26
+        }
+        style = {
+          {
+            color: tintColor
+          }
+        }
         />
       ),
     }
@@ -73,20 +126,52 @@ const TABS = {
 }
 
 type Props = {};
-export default class DynamicTabNavigator extends Component<Props> {
-  constructor(props){
+export default class DynamicTabNavigator extends Component < Props > {
+  constructor(props) {
     super(props);
-    // console.disableYellowBox = true;
+    console.disableYellowBox = true;
   }
-  _tabNavigator(){
-    const {PopularPage,TrendingPage, FavoritePage, MyPage} = TABS;
-    const tabs = {PopularPage,TrendingPage, FavoritePage};//根据需要定制显示的tab
-    return createAppContainer(createBottomTabNavigator(tabs,));
+  _tabNavigator() {
+    const {
+      PopularPage,
+      TrendingPage,
+      FavoritePage,
+      MyPage
+    } = TABS;
+    const tabs = {
+      PopularPage,
+      TrendingPage,
+      FavoritePage,
+      MyPage
+    }; //根据需要定制显示的tab
+    PopularPage.navigationOptions.tabBarLabel = '首页'; //动态配置页面属性
+    return createAppContainer(createBottomTabNavigator(tabs, {
+      tabBarComponent: TabBarComponent
+    }));
   }
   render() {
     NavigationUtil.navigation = this.props.navigation;
     const Tab = this._tabNavigator();
-    return <Tab/>
+    return <Tab / >
+  }
+}
+
+class TabBarComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.theme = {
+      tintColor: props.activeTintColor,
+      updateTime: new Date().getTime(),
+    }
+  }
+  render() {
+    return <BottomTabBar {
+        ...this.props
+    }
+    activeTintColor = {
+      this.theme.tintColor || this.props.activeTintColor
+    }
+    />
   }
 }
 
